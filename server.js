3d90264660fd76fa6b826645552b74dbb361179b
EDLA KOUSHIK <koushikedla01@gmail.com>
2025-05-19 09:31:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "https://prepwise-frontend.vercel.app/",
   credentials: true
 }));
 
